91fb9be1bd42b5b51386920c445c7048f4e3fdb7
Joseph Mwanzia <makamwanzia@gmail.com>
2024-12-19 15:14:23 +0300

    feat: receive response data from API

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+const express = require('express')
+const app = express()
+const axios = require('axios')
 require('dotenv').config()
-const WEATHER_API = process.env.WEATHER_API
+const API_KEY = process.env.API_KEY
+const PORT = process.env.PORT
 
-  fetch(WEATHER_API)
-    .then(response => response.json())
-    .then((data => {
-      console.dir(data, {depth: null})
-    }))
-    .catch(err => {
-      console.error('Error Fetching Data: ', err);
-    })
+app.get("/weather/:city", async (req, res) => {
+  const {city} = req.params
+  
+  try {
+    const response = await axios(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&include=hours%2Cevents&key=${API_KEY}&contentType=json`)
+    res.send(response.data)
+  } catch (error) {
+    console.log('Error fetching data from weather API: ',error)
+    throw new Error('Failed to fetch weather data')
+  }
+})
 
+app.listen(PORT, () => {
+  console.log(`App listening @ http://localhost:${PORT}`)
+})
